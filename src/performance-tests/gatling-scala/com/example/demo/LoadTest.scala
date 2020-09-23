691c515e88e9ede7b4c46dbb88f409151089f801
Your Name <you@example.com>
2020-09-23 05:55:26 +0200

    wip

--- src/performance-tests/gatling-scala/com/example/demo/LoadTest.scala
+++ src/performance-tests/gatling-scala/com/example/demo/LoadTest.scala
@@ -1,40 +1,40 @@
-package com.example.demo
-
-;
-
-import io.gatling.core.Predef._
-import io.gatling.http.Predef._
-
-import scala.util.Random
-import scala.concurrent.duration.{DurationInt, DurationLong};
-
-class LoadTest extends Simulation {
-
-  val peakLoad = 10;
-  val rampUpSeconds = 10;
-  val totalDurationSeconds = 60;
-  val maxThinkTimeMillis = 500;
-
-  val httpProtocols = http
-    .baseUrl("http://localhost:8080")
-    .acceptHeader("text/html,application/xhtml+xml,application/xml,application/json;q=0.9,*/*;q=0.8")
-    .doNotTrackHeader("1")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .acceptEncodingHeader("gzip, deflate")
-    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
-
-  val random = new Random();
-
-  val scn = scenario("actuator health").forever("loopCount") {
-    exec(
-      http("/actuator/health")
-        .get("/actuator/health")
-    ).pause(0, maxThinkTimeMillis.milliseconds)
-  }
-
-  setUp(scn.inject(
-    rampUsers(peakLoad) during (rampUpSeconds seconds)
-  ))
-    .protocols(httpProtocols)
-    .maxDuration(totalDurationSeconds seconds)
-}
+//package com.example.demo
+//
+//;
+//
+//import io.gatling.core.Predef._
+//import io.gatling.http.Predef._
+//
+//import scala.concurrent.duration.DurationInt
+//import scala.util.Random;
+//
+//class LoadTest extends Simulation {
+//
+//  val peakLoad = 10;
+//  val rampUpSeconds = 10;
+//  val totalDurationSeconds = 600;
+//  val maxThinkTimeMillis = 500;
+//
+//  val httpProtocols = http
+//    .baseUrl("http://localhost:8080")
+//    .acceptHeader("text/html,application/xhtml+xml,application/xml,application/json;q=0.9,*/*;q=0.8")
+//    .doNotTrackHeader("1")
+//    .acceptLanguageHeader("en-US,en;q=0.5")
+//    .acceptEncodingHeader("gzip, deflate")
+//    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+//
+//  val random = new Random();
+//
+//  val scn = scenario("actuator health").forever("loopCount") {
+//    exec(
+//      http("/actuator/health")
+//        .get("/actuator/health")
+//    ).pause(0, maxThinkTimeMillis.milliseconds)
+//  }
+//
+//  setUp(scn.inject(
+//    rampUsers(peakLoad) during (rampUpSeconds seconds)
+//  ))
+//    .protocols(httpProtocols)
+//    .maxDuration(totalDurationSeconds seconds)
+//}
